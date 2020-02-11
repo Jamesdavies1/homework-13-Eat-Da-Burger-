f1f4cc8dc54b1b1180372db0c20d77c8dbd43c16
james davies <james.davies92@icloud.com>
2020-02-11 10:37:25 +0000

    server.js has been tidied up

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 const express = require("express");
+const expressHandlebars = require("express-handlebars");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// Parse application body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 const routes = require("./controllers/controller.js");
